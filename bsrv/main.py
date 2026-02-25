261c409cc8aa2dc6f29d7567780cc43f65ea0f72
BlockMaster <blockmaster350@gmail.com>
2026-02-25 10:48:23 +0300

    Add more documentation, robots.txt, start script

--- bsrv/main.py
+++ bsrv/main.py
@@ -14,7 +14,7 @@ import bsrv.logic as lg
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[
                         logging.FileHandler("api.log", encoding="utf-8"),
                         logging.StreamHandler()
@@ -22,78 +22,92 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(32))
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-app = fastapi.FastAPI(title="BlockServices")
+app = fastapi.FastAPI(title="BlockServices",
+                      description="BlockServices API and alternative Scratch API for Dashblocks.",
+                      version="0.2.0")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
-    allow_headers=["Content-Type", "Authorization", "X-API-Key"]
+    allow_headers=["Content-Type", "Authorization"]
 )
 
 security = HTTPBearer()
 
+
 class UserCreate(BaseModel):
-    username: str = Field(min_length=3, max_length=20, pattern="^[a-zA-Z0-9_]+$")
-    name: str = Field(min_length=1, max_length=50, pattern="^[a-zA-Z0-9_]+$")
+    username: str = Field(min_length=3, max_length=20, pattern="^[a-zA-Z0-9_]+$", description="Unique username")
+    name: str = Field(min_length=1, max_length=50, pattern="^[a-zA-Z0-9_ ]+$", description="Display name")
     email: str = Field(pattern=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
     password: str = Field(min_length=8, max_length=100)
 
+
 class UserLogin(BaseModel):
-    username: str
+    username: str = Field(description="Unique username")
     password: str
 
 
-async def get_current_user(credentials: HTTPAuthorizationCredentials = fastapi.Depends(security)) ->  int:
+async def get_current_user(token) ->  int:
     credentials_exception = fastapi.HTTPException(
             status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"}
             )
     try:
-        uid = lg.get_uid_from_token(credentials.credentials, SECRET_KEY)
+        uid = lg.get_uid_from_token(token, SECRET_KEY)
         if uid is None:
             raise credentials_exception
     except InvalidTokenError:
         raise credentials_exception
-    return uid
+    return int(uid)
 
 
-@app.get("/")
+@app.get("/", summary="Test path. Returns hello message")
 async def root():
     return {"message": "Hello World", "status": "ok"}
 
 
-@app.post("/dash/auth/register")
+@app.post("/dash/auth/register", summary="Register new user. Needs username (unique), password, name and email. "
+                                         "Returns id of new user. After that you need to get JWT token via "
+                                         "logining in.")
 async def register(user: UserCreate):
-    logger.log(1, f"Register start {user.username}, {user.name}, {user.email}, {user.password}", exc_info=True)
+    logger.log(20, f"Register start {user.username}, {user.name}, {user.email}, {user.password}", exc_info=True)
     try:
         uid = lg.register_user(user.password, user.username, user.name, user.email)
     except UserAlreadyExists:
         raise fastapi.HTTPException(status_code=400, detail="User already exists")
-    except Exception as e:
-        raise fastapi.HTTPException(status_code=400, detail=str(e))
     return {"uid": uid}
 
-@app.post("/dash/auth/login")
+
+@app.post("/dash/auth/login", summary="Login into account. Needs username and password. Returns JWT token.")
 async def login(user: UserLogin):
+    logger.log(20, f"Login start {user.username}")
     if lg.check_password(user.password, user.username):
         try:
             uid = lg.get_user_id(user.username)
         except UserDoesntExist:
             raise fastapi.HTTPException(status_code=400, detail="User doesn't exist")
-        access_token = lg.create_access_token({"uid": uid}, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY)
+        access_token = lg.create_access_token({"uid": uid}, os.getenv("TOKEN_EXPIRE_MINUTES"), SECRET_KEY)
+        logger.log(20, f"Issued JWT token for {user.username} with id {uid}")
         return {"access_token": access_token, "token_type": "bearer"}
     else:
         raise fastapi.HTTPException(status_code=400, detail="Incorrect username or password")
 
-@app.post("/dash/auth/whoami")
-async def whoami(credentials: HTTPAuthorizationCredentials = fastapi.Depends(security)):
+
+@app.get("/dash/auth/whoami", summary="Endpoint for JWT token testing. Needs JWT token in 'token' URL parameter. "
+                                       "Returns the id of user from token.")
+async def whoami(token: str):
     try:
-        uid = await get_current_user(credentials)
+        uid = await get_current_user(token)
     except InvalidTokenError:
         raise fastapi.HTTPException(status_code=400, detail="Incorrect username or password")
     return {"username": uid}
+
+
+@app.get("/robots.txt")
+async def robots(request: fastapi.Request):
+    logger.log(20, f"Robot with ip {request.client.host}")
+    return {"Content-Type": "text/plain", "body": "User-agent: *\nDisallow: /"}
---
+++ start.py
@@ -0,0 +1,13 @@
+import uvicorn
+from  bsrv.main import logger
+import os
+
+if __name__ == "__main__":
+    logger.log(20, "Starting server")
+    if bool(os.getenv("DEBUG", False)):
+        logger.log(20, "Debug mode enabled")
+        uvicorn.run("bsrv.main:app", reload=True, log_level="debug")
+    else:
+        logger.log(20, "Running in production mode")
+        uvicorn.run("bsrv.main:app", host="0.0.0.0", port=8443,
+                    ssl_keyfile=os.getenv("PRIVKEY_PATH"), ssl_certfile=os.getenv("FULLCHAIN_PATH"))
